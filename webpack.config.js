dbca30d06a864f411eeb5487540556cd363e4a67
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-11-11 09:50:57 -0500

    add visualizer and analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].js",
-    chunkFilename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: ["syntax-dynamic-import"],
-          },
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        // User styles
-        test: /\.(css|less|s[ac]ss|styl)$/,
-        use: [
-          {
-            loader: "css-loader",
-            options: {
-              url: true,
-              sourceMap: true,
-              modules: {
-                mode: "local",
-                localIdentName: "[local]__[hash:base64:5]",
-              },
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              plugins: [autoprefixer()],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        loader: "url-loader",
-      },
-      {
-        test: /\.(png|jpg|gif|svg|mp3)$/,
-        loader:
-          process.env.NODE_ENV === "production" ? "file-loader" : "url-loader",
-      },
-    ],
-  },
-  plugins: [
+module.exports = (env) => {
+  const plugins = [
     new CleanWebpackPlugin({
       verbose: true,
     }),
@@ -73,40 +12,112 @@ module.exports = {
       title: "Webpack",
       template: "./public/index.html",
     }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    open: true,
-    port: 3000,
-    host: "0.0.0.0",
-  },
-  devtool:
-    process.env.NODE_ENV === "development"
-      ? "eval-cheap-module-source-map"
-      : "source-map",
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        common: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "common",
-          chunks: "all",
-          filename: "[name].js",
+  ];
+
+  if (env.NODE_ENV === "analyze") {
+    const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+      .BundleAnalyzerPlugin;
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+  return {
+    entry: "./src/index.js",
+    output: {
+      filename: "[name].js",
+      chunkFilename: "[name].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              plugins: ["syntax-dynamic-import"],
+            },
+          },
         },
-        app: {
-          test: /[\\/]src[\\/]/,
-          name: "app",
-          chunks: "all",
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          // User styles
+          test: /\.(css|less|s[ac]ss|styl)$/,
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                url: true,
+                sourceMap: true,
+                modules: {
+                  mode: "local",
+                  localIdentName: "[local]__[hash:base64:5]",
+                },
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+                plugins: [autoprefixer()],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          loader: "url-loader",
+        },
+        {
+          test: /\.(png|jpg|gif|svg|mp3)$/,
+          loader:
+            process.env.NODE_ENV === "production"
+              ? "file-loader"
+              : "url-loader",
+        },
+      ],
+    },
+    plugins,
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      open: true,
+      port: 3000,
+      host: "0.0.0.0",
+    },
+    devtool:
+      process.env.NODE_ENV === "development"
+        ? "eval-cheap-module-source-map"
+        : "source-map",
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          common: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "common",
+            chunks: "all",
+            filename: "[name].js",
+          },
+          app: {
+            test: /[\\/]src[\\/]/,
+            name: "app",
+            chunks: "all",
+          },
         },
       },
+      providedExports: process.env.NODE_ENV !== "production",
+      minimize: process.env.NODE_ENV === "production",
     },
-    providedExports: process.env.NODE_ENV !== "production",
-    minimize: process.env.NODE_ENV === "production",
-  },
-  performance:
-    process.env.NODE_ENV === "production"
-      ? false
-      : {
-          hints: "warning",
-        },
+    performance:
+      process.env.NODE_ENV === "production"
+        ? false
+        : {
+            hints: "warning",
+          },
+  };
 };
